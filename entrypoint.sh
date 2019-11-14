4d6654a55ca46818d6fd7d9c69581faed0989719
albertalvarez <albert@alvarezbruned.com>
2019-11-14 22:37:50 +0100

    remove file

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while :;
-  do
-  inotifywait -m /home/seluser/Downloads/ -e create 
-  #@TODO= ejecutar deepspeech del fichero descargado, generar string en otra ruta, borrar fichero de nuevo
-
-done
